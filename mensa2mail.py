a57ce59f8b77608b1238cee7c850b06b446b1321
matthias <mame@plattform32.de>
2017-10-03 21:07:59 +0200

    date is today

--- mensa2mail.py
+++ mensa2mail.py
@@ -1,16 +1,23 @@
 import os
+import datetime
 import requests
 from jinja2 import Template, Environment, FileSystemLoader
 headers = {'User-Agent': 'mensa2mail 0.1',}
 mypath = os.path.dirname(os.path.abspath(__file__))
 
-date="2017-07-04"
+d = datetime.datetime.today()
+date=d.strftime('%Y-%m-%d')
+#date="2017-07-04"
 jsonCanteen = requests.get("https://openmensa.org/api/v2/canteens/372/", headers=headers)
+jsonCanteenDay = requests.get("https://openmensa.org/api/v2/canteens/372/days/"+date, headers=headers)
 jsonMeals = requests.get("https://openmensa.org/api/v2/canteens/372/days/"+date+"/meals", headers=headers)
 canteen = jsonCanteen.json()
+canteenDay = jsonCanteenDay.json()
 meals = jsonMeals.json()
+date=d.strftime('%d.%m.')
 env = Environment(loader=FileSystemLoader(mypath), trim_blocks=True)
-print(env.get_template('template.txt').render(date=date, canteen=canteen, menues=meals))
+if not canteenDay['closed']:
+    print(env.get_template('template.txt').render(date=date, canteen=canteen, menues=meals))
 # https://stackoverflow.com/questions/19627911/how-to-see-if-a-string-contains-another-string-in-django-template
 #TODO: CLI
 #TODO: trigger only if open -> fallbackCanteen
