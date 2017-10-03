cf492fb6ef115d5196cc47dbdf124cad0e0878b3
matthias <mame@plattform32.de>
2017-10-03 21:36:09 +0200

    seperate method

--- mensa2mail.py
+++ mensa2mail.py
@@ -2,24 +2,28 @@ import os
 import datetime
 import requests
 from jinja2 import Template, Environment, FileSystemLoader
+
+mensaID = "372"
+
 headers = {'User-Agent': 'mensa2mail 0.1',}
 mypath = os.path.dirname(os.path.abspath(__file__))
+
+def getData(mensaID, date):
+    jsonCanteen = requests.get("https://openmensa.org/api/v2/canteens/"+mensaID+"/", headers=headers)
+    jsonCanteenDay = requests.get("https://openmensa.org/api/v2/canteens/"+mensaID+"/days/"+date, headers=headers)
+    jsonMeals = requests.get("https://openmensa.org/api/v2/canteens/"+mensaID+"/days/"+date+"/meals", headers=headers)
+    canteen = jsonCanteen.json()
+    canteenDay = jsonCanteenDay.json()
+    meals = jsonMeals.json()
+    return canteen, canteenDay['closed'], meals
 
 d = datetime.datetime.today()
 date=d.strftime('%Y-%m-%d')
 #date="2017-07-04"
-jsonCanteen = requests.get("https://openmensa.org/api/v2/canteens/372/", headers=headers)
-jsonCanteenDay = requests.get("https://openmensa.org/api/v2/canteens/372/days/"+date, headers=headers)
-jsonMeals = requests.get("https://openmensa.org/api/v2/canteens/372/days/"+date+"/meals", headers=headers)
-canteen = jsonCanteen.json()
-canteenDay = jsonCanteenDay.json()
-meals = jsonMeals.json()
-date=d.strftime('%d.%m.')
-env = Environment(loader=FileSystemLoader(mypath), trim_blocks=True)
-if not canteenDay['closed']:
+canteen, open, meals = getData(mensaID, date)
+if open :
+    date = d.strftime('%d.%m.')
+    env = Environment(loader=FileSystemLoader(mypath), trim_blocks=True)
     print(env.get_template('template.txt').render(date=date, canteen=canteen, menues=meals))
-# https://stackoverflow.com/questions/19627911/how-to-see-if-a-string-contains-another-string-in-django-template
 #TODO: CLI
-#TODO: trigger only if open -> fallbackCanteen
 #TODO: sendmail
-#TODO: kürzeres Datum
---
+++ readme.md
@@ -0,0 +1,9 @@
+A simple Python script to mail daily canteen menu by www.openmensa.org
+
+Uses the json APIv2 of OpenMensa and is build upon Python3 and requests, jinja2 libs.
+Sends mails like this example:
+`TEST TEST TEST`
+
+#Install
+
+#Config
